f29b9e50b9b787a5797f86589106a3e5b286c00d
Rahul-iOS-ttn <rahul.sharma1@tothenew.com>
2021-04-17 22:39:09 +0530

    Login WireFrame

--- Media_final_project/Media_final_project/ViewController.swift
+++ Media_final_project/Media_final_project/ViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    
+    var managedObjectContext: NSManagedObjectContext!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 //changes
     
 }
